88397b861c578b7a8ae80b318e9b1ad1d120994e
Anshul Choudhary <achoudhary718@gmail.com>
2020-04-30 15:27:58 +0530

    php update

--- XAMPP htdocs folder files/queries.php
+++ XAMPP htdocs folder files/queries.php
@@ -2,6 +2,25 @@
 	require "conn.php";
 	$type=$_POST["type"];
 
+	function singlequery($result){
+		$row=mysqli_fetch_row($result);
+		$string = implode(':', $row);
+		return "$string";
+	}
+
+	function multiplequery($result){
+		$string="";
+		$seperate=";";
+		while($row=mysqli_fetch_row($result)){
+			$temp = implode(':', $row);
+			$string = $string.$temp;
+			$string = $string.$seperate;
+		}
+		return "$string";
+		
+	}
+
+
 	if($type == "login"){
 		$username=$_POST["username"];
 		$password=$_POST["password"];
@@ -54,6 +73,135 @@
 		}
 		else{
 			echo "signup failure";
+		}
+	}
+	else if($type == "home"){
+		$username=$_POST["username"];
+		//$user_name="u1";
+		//$user_pass="123";
+		$mysql_qry="select * from user as u, user_phone_number as upn where u.User_ID = '$username' and u.User_ID = upn.User_ID;";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		if(mysqli_num_rows($result) > 0){
+			echo singlequery($result);
+		}
+		else{
+			echo "user details access failure";
+		}
+	}
+	else if($type == "addbook"){
+		$username=$_POST["username"];
+		$isbn = $_POST["isbn"];
+		$repaymethod = $_POST["repaymethod"];
+		$availability = $_POST["availability"];
+		$otherspec = $_POST["otherspec"];
+		$securitymoney = $_POST["securitymoney"];
+
+		$url="https://www.googleapis.com/books/v1/volumes?q=isbn:";
+		$url=$url.$isbn;
+
+		$str = file_get_contents($url);	
+		$json = json_decode($str, true);
+
+		if($json['totalItems'] == 0){
+			echo "Book not available ISBN database";
+		}
+		else{
+			if(array_key_exists("title",$json['items'][0]['volumeInfo'])){
+				$bookname = $json['items'][0]['volumeInfo']['title'];
+			}
+			else{
+				$bookname = "";
+			}
+			if(array_key_exists("publishedDate",$json['items'][0]['volumeInfo'])){
+				$year = $json['items'][0]['volumeInfo']['publishedDate'];
+			}
+			else{
+				$year = "";
+			}
+			if(array_key_exists("authors",$json['items'][0]['volumeInfo'])){
+				$author = join(", ",$json['items'][0]['volumeInfo']['authors']);
+			}
+			else{
+				$author = "";
+			}
+			if(array_key_exists("language",$json['items'][0]['volumeInfo'])){
+				$language = $json['items'][0]['volumeInfo']['language'];
+			}
+			else{
+				$language = "";
+			}		
+			
+			//$user_name="u1";
+			//$user_pass="123";
+			$mysql_qry="insert into book values('$isbn','$bookname','$year','$author','$language');";
+			mysqli_query($conn ,$mysql_qry);
+
+			//if($result == FALSE){
+			//	echo "insert into book failed";
+			//}
+			
+			$mysql_qry="insert into my_books values('$repaymethod','$availability','$otherspec','$securitymoney','$username','$isbn');";
+			$result=mysqli_query($conn ,$mysql_qry);
+
+			if($result == FALSE){
+				echo "Book already added!";
+			}
+			else{
+				echo "Book Added Successfully";
+			}	
+		}	
+	}
+	else if($type == "mybooks"){
+		$username=$_POST["username"];
+		//$user_name="u1";
+		//$user_pass="123";
+		$mysql_qry="select * from my_books as mb, book as b where mb.User_ID = '$username' and mb.ISBN = b.ISBN;";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		if(mysqli_num_rows($result) > 0){
+			echo multiplequery($result);
+		}
+		else{
+			echo "No Book Added!";
+		}
+	}
+	else if($type == "updatebook"){
+		$username=$_POST["username"];
+		$isbn = $_POST["isbn"];
+		$repaymethod = $_POST["repaymethod"];
+		$availability = $_POST["availability"];
+		$otherspec = $_POST["otherspec"];
+		$securitymoney = $_POST["securitymoney"];
+
+		$mysql_qry="update my_books set Repayment_Policy='$repaymethod', Availability = '$availability',Other_Specifications = '$otherspec', Security_Money_of_Book = '$securitymoney' where User_ID = '$username' and ISBN = '$isbn';";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		$mysql_qry="select * from my_books as mb, book as b where mb.User_ID = '$username' and mb.ISBN = b.ISBN;";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		if(mysqli_num_rows($result) > 0){
+			echo multiplequery($result);
+		}
+		else{
+			echo "No Book Added!";
+		}
+	}
+	else if($type == "deletebook"){
+		$username=$_POST["username"];
+		$isbn = $_POST["isbn"];
+
+		$mysql_qry="delete from my_books where User_ID = '$username' and ISBN = '$isbn';";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		$mysql_qry="select * from my_books as mb, book as b where mb.User_ID = '$username' and mb.ISBN = b.ISBN;";
+		$result=mysqli_query($conn ,$mysql_qry);
+
+		if(mysqli_num_rows($result) > 0){
+			echo multiplequery($result);
+		}
+		else{
+			echo "No Book Added!";
 		}
 	}
 		
